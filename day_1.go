88e3b76824c0360ef8f8185b212846a72cde2180
arisal2 <abhinavrisal99@gmail.com>
2022-12-01 16:07:24 +0545

    day 1 solution

---
+++ day_1.go
@@ -0,0 +1,56 @@
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const filePath = "inputs/day_1.txt"
+
+func main() {
+	//filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []int
+	var totalCalories []int
+
+	for fileScanner.Scan() {
+		convertedValue, _ := strconv.Atoi(fileScanner.Text())
+		fileLines = append(fileLines, convertedValue)
+	}
+
+	readFile.Close()
+
+	total := 0
+	for _, line := range fileLines {
+		if line == 0 {
+			totalCalories = append(totalCalories, total)
+			total = 0
+		} else {
+			total += line
+		}
+	}
+
+	sort.Ints(totalCalories)
+
+	// Part 1
+	fmt.Println("Elf with highest calories:", totalCalories[len(totalCalories)-1])
+
+	// Part 2
+	topThreeElves := 0
+	for i := len(totalCalories) - 1; i >= len(totalCalories)-3; i-- {
+		topThreeElves += totalCalories[i]
+	}
+
+	fmt.Println("Total of top three elves:", topThreeElves)
+}
