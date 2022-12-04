14a372daf41465d5efe285c9137020068fd24618
arisal2 <abhinavrisal99@gmail.com>
2022-12-04 18:05:29 +0545

    Finish day 3 part 2

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -3,6 +3,8 @@ package main
 import (
 	fr "advent-of-code/fileReader"
 	"fmt"
+	"sort"
+	"strings"
 	"unicode"
 )
 
@@ -14,30 +16,67 @@ func main() {
 	fileLines := fr.Reader()
 
 	for _, lines := range fileLines {
-		mid = (len(lines) / 2) - 1
-		end = len(lines) - 1
-		total += compareRucksacks(lines[0:mid+1], lines[mid+1:end+1])
+		mid = (len(lines) / 2)
+		end = len(lines)
+		total += compareRucksacks(lines[0:mid], lines[mid:end])
+	}
+
+	for i := 0; i < len(fileLines)-1; i = i + 3 {
+		groupTotal += findCommon(sortString(fileLines[i]), sortString(fileLines[i+1]), sortString(fileLines[i+2]))
 	}
 
 	// PART 1
 	fmt.Printf("The priority total is: %d", total)
+	fmt.Println()
 
 	// PART 2
 	fmt.Printf("The group total is: %d", groupTotal)
 }
 
-func compareRucksacks(ruckSackFirst, ruckSackSecond string) (result int) {
-	for _, first := range ruckSackFirst {
-		for _, second := range ruckSackSecond {
+// Need to update this method with above
+func compareRucksacks(rsOne, rsTwo string) (result int) {
+	for _, first := range rsOne {
+		for _, second := range rsTwo {
 			if first == second {
-				if unicode.IsUpper(first) {
-					result = (int(first) + 27) - 65
-				} else {
-					result = int(first) - 96
-				}
-				break
+				return checkUnicode(first)
 			}
 		}
 	}
+
 	return
 }
+
+func sortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+func checkUnicode(first rune) (result int) {
+	if unicode.IsUpper(first) {
+		return (int(first) + 27) - 65
+	} else {
+		return int(first) - 96
+	}
+}
+
+func findCommon(rsOne, rsTwo, rsThree string) (result int) {
+	var i, j, k = 0, 0, 0
+	for i < len(rsOne) && j < len(rsTwo) && k < len(rsThree) {
+		if rsOne[i] == rsTwo[j] && rsTwo[j] == rsThree[k] {
+			result = checkUnicode(rune(rsOne[i]))
+			i++
+			j++
+			k++
+		} else if rsOne[i] < rsTwo[j] {
+			i++
+		} else if rsTwo[j] < rsThree[k] {
+			j++
+		} else {
+			k++
+		}
+	}
+
+	return
+}
+
